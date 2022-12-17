11caacaf1ac050493dde3339a821db3c6a99276e
Leonardo_13 <leonardo.novak5@gmail.com>
2022-12-17 15:08:40 +0100

    StudentViewModel and StudentViewModelFactory class created

--- app/src/main/java/com/optimahorizonapps/studentregister/MainActivity.kt
+++ app/src/main/java/com/optimahorizonapps/studentregister/MainActivity.kt
@@ -2,10 +2,36 @@ package com.optimahorizonapps.studentregister
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
+import com.optimahorizonapps.studentregister.db.StudentDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var nameEditText: EditText
+    private lateinit var emailEditText: EditText
+    private lateinit var saveButton: Button
+    private lateinit var clearButton: Button
+
+    private lateinit var viewModel: StudentViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nameEditText = findViewById(R.id.name_et)
+        emailEditText = findViewById(R.id.email_et)
+        saveButton = findViewById(R.id.save_btn)
+        clearButton = findViewById(R.id.clear_btn)
+
+        val dao = StudentDatabase.getInstance(application).studentDao()
+        val factory = StudentViewModelFactory(dao)
+        viewModel = ViewModelProvider(this)[StudentViewModel::class.java]
+
+    }
+
+    private fun saveStudentData() {
+
     }
 }
---
+++ app/src/main/java/com/optimahorizonapps/studentregister/StudentViewModelFactory.kt
@@ -0,0 +1,11 @@
+package com.optimahorizonapps.studentregister
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.optimahorizonapps.studentregister.db.StudentDao
+
+class StudentViewModelFactory(private val dao: StudentDao): ViewModelProvider.Factory {
+
+
+
+}
